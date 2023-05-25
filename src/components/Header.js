3e6c0c34c4fcfeee0d20759bdfd8854d11d75849
Lekuba Ntoane <l.ntoane@gmail.com>
2023-05-26 01:55:35 +0200

    Hide button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
 import Button from './Button'
+import { useLocation } from 'react-router-dom'
 
 const Header = ({ title, onAdd, showAdd }) => {
+
+  // This Hook gives us access to path name
+  const location = useLocation()
 
   return (
     <div className='header'>
       <h1>{title}</h1>
       {/* showAdd ? is the same as showAdd == true ? */}
-      <Button
+      {location.pathname == '/' && <Button
         color={showAdd ? 'red' : 'green'}
         text={showAdd ? 'Close' : 'Add'}
-        onClick={onAdd} />
+        onClick={onAdd}
+      />}
     </div>
   )
 }
